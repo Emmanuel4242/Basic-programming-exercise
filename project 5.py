7d5b0c4891680937d6fd74ffecbe3ef83e1b57de
Emmanuel4242 <160818076+Emmanuel4242@users.noreply.github.com>
2024-03-07 14:21:51 +0700

    Create project 5.py

---
+++ project 5.py
@@ -0,0 +1,13 @@
+# input two intergers
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enterthe second integer: "))
+
+# compare the two integers
+if num1 == num2:
+  print("The two integers are equal.")
+elif num1 > num2:
+  print("The first integer is greater than the second integer.")
+else:
+  print("The first integer is smaller than the second integer.")  
+  
+           
