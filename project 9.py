bf278f6cb69c51d22cc5736391fa216fb1567b07
Emmanuel4242 <160818076+Emmanuel4242@users.noreply.github.com>
2024-03-07 14:40:27 +0700

    Create project 9.py

---
+++ project 9.py
@@ -0,0 +1,8 @@
+# input the user's age
+age = int(input("21: "))
+
+# check if the age is not less than 21
+if age >= 21:
+  print("you are of legal age.")
+else:
+  print("you are not of legal age.")
