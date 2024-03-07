65defbf37307e3b00614de40d4ae5675ab17bab5
Emmanuel4242 <160818076+Emmanuel4242@users.noreply.github.com>
2024-03-07 14:54:49 +0700

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,20 @@
+#string variable
+name= "Deng kuot"
+#string variable
+age= 23
+# boolean variable
+ismale= True
+
+#assign one variable to multiple variable
+a=b=c=1
+#re assign c variable 
+c=2 
+d="d"
+print("a is: ",a)
+prit("b is: ",b)
+print("c is: ",c)
+print("d is: ",d)
+
+# print ("my name is :",name)
+# print ("my age is :",age)
+# print ("i am a male:",ismale)
